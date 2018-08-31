71761760990adad7ccffd9338d12ebd0dc6eefd5
jiyinyiyong <jiyinyiyong@gmail.com>
2018-09-01 02:07:32 +0800

    switching to card style

--- src/app/comp/home.cljs
+++ src/app/comp/home.cljs
@@ -26,49 +26,61 @@
    comp-copied
    states
    (:content snippet)
-   (pre
-    {:style (merge
-             ui/flex
-             ui/textarea
-             {:font-family ui/font-code,
-              :min-height 80,
-              :margin 0,
-              :white-space :pre-wrap,
-              :word-break :break-all}),
-     :inner-text (:content snippet)}))
-  (div
-   {:style (merge {:padding 8})}
-   (cursor->
-    :confirm
-    comp-confirm
-    states
-    {:trigger (span
-               {:style {:cursor :pointer, :color (hsl 0 80 80)}}
-               (comp-icon :ios-trash)),
-     :text (println (.-tagName (.-activeElement js/document)))}
-    (fn [e d! m!] (d! :snippet/remove-one (:id snippet)))))))
+   (div
+    {:style {:position :relative}}
+    (pre
+     {:style (merge
+              ui/flex
+              {:font-family ui/font-code,
+               :min-height 80,
+               :margin 0,
+               :white-space :pre-wrap,
+               :word-break :break-all,
+               :border (str "1px solid " (hsl 0 0 90)),
+               :padding 16}),
+      :inner-text (:content snippet)})
+    (cursor->
+     :confirm
+     comp-confirm
+     states
+     {:trigger (div
+                {:style (merge
+                         {:position :absolute,
+                          :bottom 0,
+                          :right 0,
+                          :padding 16,
+                          :background-color (hsl 0 0 0 0.02),
+                          :border-radius 8})}
+                (span
+                 {:style {:cursor :pointer, :color (hsl 0 80 80)}}
+                 (comp-icon :ios-trash))),
+      :text (println (.-tagName (.-activeElement js/document)))}
+     (fn [e d! m!] (d! :snippet/remove-one (:id snippet))))))))
 
 (defcomp
  comp-home
  (states snippets)
  (let [state (or (:data states) {:content ""}), content (:content state)]
    (div
-    {:style (merge ui/flex {:padding "24px 16px 240px 16px", :overflow :auto})}
+    {:style (merge
+             ui/flex
+             {:padding "24px 16px 240px 16px",
+              :overflow :auto,
+              :background-color (hsl 0 0 97)})}
     (div
-     {:style ui/column}
+     {:style {:position :relative}}
      (textarea
       {:value content,
        :style (merge
                ui/flex
                ui/textarea
-               {:min-height 80, :font-family ui/font-code, :overflow :auto}),
+               {:min-height 80, :font-family ui/font-code, :overflow :auto, :width "100%"}),
        :class-name schema/box-name,
        :on-input (mutation-> (assoc state :content (:value %e)))})
-     (=< nil 8)
      (div
-      {}
-      (button
-       {:style style/button,
+      {:style {:position :absolute, :right 8, :bottom 8}}
+      (a
+       {:style style/link,
         :on-click (fn [e d! m!]
           (when (not (string/blank? content))
             (d! :snippet/create content)
